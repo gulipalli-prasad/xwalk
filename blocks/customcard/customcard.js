232a103686157657d64ff9b08fa7d1cc78b5a150
gulipalli-prasad <gulipalli.prasad@nagarro.com>
2024-07-04 13:01:22 +0530

    added fields in component-models.json

--- blocks/customcard/customcard.js
+++ blocks/customcard/customcard.js
@@ -8,15 +8,6 @@ export default function decorate(block) {
       detailedTextEl,
     ] = block.children;
 
-    // const image = imageSourceEl?.querySelector("picture");
-    // if (image) {
-    //   const img = image.querySelector("img");
-    //   img.removeAttribute("width");
-    //   img.removeAttribute("height");
-    //   const alt = "image";
-    //   img.setAttribute("alt", alt);
-    // }
-
     const titleHeading = titleHeadingEl?.textContent?.trim();
     const titleDescription = titleDescriptionEl?.textContent?.trim();
     const textContent = textContentEl?.textContent?.trim();
@@ -34,32 +25,32 @@ export default function decorate(block) {
 
   const customCard = getCustomCard(block);
 
-  // // Create the form elements
-  // const responseDiv = document.createElement("div");
-  // responseDiv.className = "card";
+  // Create the form elements
+  const responseDiv = document.createElement("div");
+  responseDiv.className = "card";
 
-  // const title = document.createElement("h2");
-  // title.textContent = customCard.titleHeading;
-  // responseDiv.appendChild(title);
+  const title = document.createElement("h2");
+  title.textContent = customCard.titleHeading;
+  responseDiv.appendChild(title);
 
-  // const description = document.createElement("h5");
-  // description.textContent = `${titleDescription}`;
-  // responseDiv.appendChild(description);
+  const description = document.createElement("h5");
+  description.textContent = `${titleDescription}`;
+  responseDiv.appendChild(description);
 
-  // const imageDiv = document.createElement("div");
-  // imageDiv.className = "faking";
-  // imageDiv.textContent = customCard.imageSource;
-  // imageDiv.style.height = "200px";
-  // responseDiv.appendChild(imageDiv);
+  const imageDiv = document.createElement("div");
+  imageDiv.className = "faking";
+  imageDiv.textContent = customCard.imageSource;
+  imageDiv.style.height = "200px";
+  responseDiv.appendChild(imageDiv);
 
-  // const paragraphText = document.createElement("p");
-  // paragraphText.textContent = customCard.textContent;
-  // responseDiv.appendChild(paragraphText);
+  const paragraphText = document.createElement("p");
+  paragraphText.textContent = customCard.textContent;
+  responseDiv.appendChild(paragraphText);
 
-  // const paragraphDescription = document.createElement("p");
-  // paragraphDescription.textContent = customCard.detailedText;
-  // responseDiv.appendChild(paragraphDescription);
+  const paragraphDescription = document.createElement("p");
+  paragraphDescription.textContent = customCard.detailedText;
+  responseDiv.appendChild(paragraphDescription);
 
-  // // Add the form to the block
-  // block.appendChild(responseDiv);
+  // Add the form to the block
+  block.appendChild(responseDiv);
 }
