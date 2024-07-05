6135863ccb3f71ce655f4afe052b0726e8e06b27
gulipalli-prasad <gulipalli.prasad@nagarro.com>
2024-07-05 15:08:17 +0530

    Update nav-bar.js

--- blocks/nav-bar/nav-bar.js
+++ blocks/nav-bar/nav-bar.js
@@ -24,8 +24,17 @@ export default function decorate(block) {
     const navBackgroundImage =
       navBgImgElement?.getAttribute("src")?.trim() || "";
 
-    const ctaLink = ctaLinkEl?.textContent?.trim() || "";
-    const ctaText = ctaTextEl?.textContent?.trim() || "";
+    //  const ctaLink = ctaLinkEl?.textContent?.trim() || "";
+    //  const ctaText = ctaTextEl?.textContent?.trim() || "";
+
+    const cta = ctaLinkEl
+      ? {
+          href: ctaLinkEl.querySelector("a")?.href || "#",
+          title: ctaLinkEl.querySelector("a")?.title || "",
+
+          textContent: ctaTextEl?.textContent?.trim() || "",
+        }
+      : null;
 
     return {
       heading,
@@ -33,27 +42,31 @@ export default function decorate(block) {
       navImage,
       altText,
       navBackgroundImage,
-      ctaLink,
-      ctaText,
+      cta,
     };
   }
 
-  const Navbar = getNavbar(block);
+  const navBar = getNavbar(block);
 
   // Create the HTML structure using template literals
-  const NavbarHtml = `
-    <div class="block-with-background" style="background-image: url('${Navbar.navBackgroundImage}');">
-	    <div class="nav-bar-heading">${Navbar.heading}</div>
-	    <div class="description">${Navbar.description}</div>
-	    <img src="${Navbar.navImage}" alt="${Navbar.altText}" class="custom-image"/>
-       <a href= "${Navbar.ctaLink}" class="custom-link">${Navbar.ctaText}</a>
-
-       
+  const navBarHtml = `
+    <div class="block-with-background" style="background-image: url('${
+      navBar.navBackgroundImage
+    }');">
+	    <div class="nav-bar-heading">${navBar.heading}</div>
+	    <div class="description">${navBar.description}</div>
+	    <img src="${navBar.navImage}" alt="${navBar.altText}" class="custom-image"/>
+      
+        <a href="${navBar.cta?.href || "#"}" title="${
+    navBar.cta?.title || ""
+  }" class="custom-link">
+          <p>${navBar.cta?.textContent}</p>
+        </a>
     </div>
   `;
 
   // Set the generated HTML to the block
-  block.innerHTML = NavbarHtml;
+  block.innerHTML = navBarHtml;
 
   // Add necessary classes
   // const headingElement = block.querySelector(".nav-bar-heading");
