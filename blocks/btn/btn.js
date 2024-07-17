6bd04244c1f6e3934de92d22eff66fbe201ff1c5
gulipalli-prasad <gulipalli.prasad@nagarro.com>
2024-07-17 14:28:17 +0530

    Update btn.js

--- blocks/btn/btn.js
+++ blocks/btn/btn.js
@@ -32,36 +32,37 @@ export default function decorate(block) {
     return modal;
   }
 
-  // async function callLogoutAPI() {
-  //   try {
-  //     const response = await fetch(
-  //       "https://jsonplaceholder.typicode.com/posts",
-  //       {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         body: JSON.stringify({ action: "logout" }),
-  //       }
-  //     );
+  async function callLogoutAPI() {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ action: "logout" }),
+        }
+      );
 
-  //     if (response.ok) {
-  //       alert("Logout successful");
-  //       // Redirect to the login page or perform another action
-  //       window.location.href = link; // Change the URL to your login page
-  //     } else {
-  //       alert("Logout failed. Please try again.");
-  //     }
-  //   } catch (error) {
-  //     console.error("Error logging out:", error);
-  //     alert("Error logging out. Please try again.");
-  //   }
-  // }
+      if (response.ok) {
+        alert("Logout successful");
+        // Redirect to the login page or perform another action
+        window.location.href = link; // Change the URL to your login page
+      } else {
+        alert("Logout failed. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error logging out:", error);
+      alert("Error logging out. Please try again.");
+    }
+  }
 
-  // function clearLocalStorage() {
-  //   localStorage.removeItem("name");
-  //   // Add more keys to remove if needed
-  // }
+  function clearLocalStorage() {
+    localStorage.removeItem("name");
+
+    // Add more keys to remove if needed
+  }
 
   function setupEventListeners(logoutButton, modal) {
     logoutButton.addEventListener("click", () => {
@@ -73,9 +74,8 @@ export default function decorate(block) {
 
     yesButton.addEventListener("click", () => {
       modal.style.display = "none";
-      // clearLocalStorage(); // Clear local storage
-      // callLogoutAPI(); // Call the API to log out
-      window.location.href = link;
+      clearLocalStorage(); // Clear local storage
+      callLogoutAPI(); // Call the API to log out
     });
 
     noButton.addEventListener("click", () => {
